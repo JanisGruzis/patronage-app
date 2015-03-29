e508a3578b5be3b74a663a742d983dec95b1ea4f
Jānis Gruzis <jgruzis@gmail.com>
2015-03-29 11:23:30 +0300

    Move to prod environment

--- aproject/controllers/stops.js
+++ aproject/controllers/stops.js
@@ -3,11 +3,11 @@ controllers.controller('Stops', ['$scope', '$http', '$routeParams', '$rootScope'
 	
 		$rootScope.gTransportId = $routeParams.transportId;
 		console.log('$routeParams', $routeParams);
-		$http.get('http://patronage.cloudapp.net/app_dev.php/rest/route/list/' + $routeParams.transportId)
+		$http.get('http://patronage.cloudapp.net/rest/route/list/' + $routeParams.transportId)
 			.success(function(data){
 				$scope.routes = data;
 				$scope.route = data[0].id;
-				$http.get('http://patronage.cloudapp.net/app_dev.php/rest/stop/list/' + $scope.route)
+				$http.get('http://patronage.cloudapp.net/rest/stop/list/' + $scope.route)
 					.success(function(data){
 						$scope.stops = data;
 					});				
@@ -16,7 +16,7 @@ controllers.controller('Stops', ['$scope', '$http', '$routeParams', '$rootScope'
 		$scope.showTime = ($routeParams.reisId)?true:false;
 		
 		if(reisId){
-			$http.get('http://patronage.cloudapp.net/app_dev.php/rest/stop/time/' + reisId)
+			$http.get('http://patronage.cloudapp.net/rest/stop/time/' + reisId)
 			.success(function(data){
 				$scope.time = data;
 			});
@@ -24,7 +24,7 @@ controllers.controller('Stops', ['$scope', '$http', '$routeParams', '$rootScope'
 /* 		$scope.$watch('route', function(newValue, oldValue){
 			console.log(this);
 			if (newValue != oldValue) {
-				$http.get('http://patronage.cloudapp.net/app_dev.php/rest/stop/list/' + $scope.route)
+				$http.get('http://patronage.cloudapp.net/rest/stop/list/' + $scope.route)
 					.success(function(data){
 						$scope.stops = data;
 					});
@@ -32,7 +32,7 @@ controllers.controller('Stops', ['$scope', '$http', '$routeParams', '$rootScope'
 		}); */
 		$scope.setDirection = function(e) {
 			//console.log('e', e.item.id);
-			$http.get('http://patronage.cloudapp.net/app_dev.php/rest/stop/list/' + e.item.id)
+			$http.get('http://patronage.cloudapp.net/rest/stop/list/' + e.item.id)
 				.success(function(data){
 					$scope.stops = data;
 				});
--- aproject/controllers/transport.js
+++ aproject/controllers/transport.js
@@ -1,7 +1,7 @@
 controllers.controller('transport', ['$scope', '$http', '$routeParams', '$rootScope',
     function ($scope, $http, $routeParams, $rootScope) {
 		$rootScope.gType = $routeParams.type;
-		$http.get('http://patronage.cloudapp.net/app_dev.php/rest/transport/list/' + $routeParams.type)
+		$http.get('http://patronage.cloudapp.net/rest/transport/list/' + $routeParams.type)
 			.success(function(data){
 				$scope.transports = data;
 			});
